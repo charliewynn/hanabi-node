eedc8a835518bf6e91e8569b865b3b643df93e16
Charlie Wynn <charlie.wynn@technivation.com>
2019-04-09 17:42:14 -0500

    fixed audit - updated test name

--- test/playTests.js
+++ test/playTests.js
@@ -1,21 +1,28 @@
-
-const hanabi = require('../source/hanabi');
-
+const hanabi = require("../source/hanabi");
 
 module.exports = {
-	setUp: function (cb) {
-		hanabi.wipeForTests();
-		cb();
-	},
-	loadingPartialDoesntAffectGame : function(test){
-		var game_init = hanabi.createGame({ playerNames: ['charlie', 'brittany'] });
-		const game = hanabi.getGame();
-		const whosTurn = game.players[game.whosTurn].id;
-		const partial = hanabi.getPlayerGameState(whosTurn);
-		console.log('partial', partial.players.map(p=>p.hand.map(c=>[c.color,c.number].join(' ')).join(' - ')).join('  --  '));
-		console.log('hands', game.players.map(p=>p.hand.map(c=>[c.color,c.number].join(' ')).join(' - ')).join('  --  '));
+  setUp: function(cb) {
+    hanabi.wipeForTests();
+    cb();
+  },
+  loadingPartialDoesntAffectGame: function(test) {
+    var game_init = hanabi.createGame({ playerNames: ["charlie", "jordan"] });
+    const game = hanabi.getGame();
+    const whosTurn = game.players[game.whosTurn].id;
+    const partial = hanabi.getPlayerGameState(whosTurn);
+    console.log(
+      "partial",
+      partial.players
+        .map(p => p.hand.map(c => [c.color, c.number].join(" ")).join(" - "))
+        .join("  --  ")
+    );
+    console.log(
+      "hands",
+      game.players
+        .map(p => p.hand.map(c => [c.color, c.number].join(" ")).join(" - "))
+        .join("  --  ")
+    );
 
-
-		test.done();
-	}
+    test.done();
+  }
 };
